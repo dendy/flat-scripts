48b5dbfba7156d2f9893253c79bc6fa666718048
Daniel Levin <dendy.ua@gmail.com>
2024-05-11 22:39:28 -0500

    generate-qtproject: Add gen example scripts

---
+++ example-gen-lib.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os.path
+import importlib.machinery
+import importlib.util
+import yaml
+
+# Assuming next typical folder structure:
+#   root_dir           - very root
+#     src_dir          - code from source control,
+#                        paths in config file will be relative to the src_dir
+#       ide_config_dir - creator ide config files
+#     local_dir        - local user configuration
+#       local.yaml
+#     project_dir      - where QtCreator .creator project file is generated
+#     build_dir        - build dir location
+
+root_dir = os.path.abspath(f'{__file__}/../..')
+src_dir = os.path.abspath(f'{root_dir}/src_dir')
+project_dir = os.path.abspath(f'{root_dir}/project_dir')
+
+with open(f'{src_dir}/local_dir/local.yaml', 'r') as f:
+	flat_scripts_dir = yaml.load(f, Loader=yaml.FullLoader)['flat_scripts_dir']
+
+loader = importlib.machinery.SourceFileLoader('generate_qtproject',
+		f'{flat_scripts_dir}/generate-qtproject.py')
+spec = importlib.util.spec_from_loader('generate_qtproject', loader)
+generate_qtproject = importlib.util.module_from_spec(spec)
+loader.exec_module(generate_qtproject)
+
+generate_qtproject.run(
+	config      = f'{src_dir}/ide_config_dir/config.yaml',
+	root_dir    = src_dir,
+	project_dir = project_dir,
+	local       = f'{src_dir}/local_dir/local.yaml')
---
+++ example-gen-proc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import argparse
+import os.path
+import subprocess
+import yaml
+
+# Assuming next typical folder structure:
+#   root_dir           - very root
+#     src_dir          - code from source control,
+#                        paths in config file will be relative to the src_dir
+#       ide_config_dir - creator ide config files
+#     local_dir        - local user configuration
+#       local.yaml
+#     project_dir      - where QtCreator .creator project file is generated
+#     build_dir        - build dir location
+
+root_dir = os.path.abspath(f'{__file__}/../..')
+src_dir = os.path.abspath(f'{root_dir}/src_dir')
+project_dir = os.path.abspath(f'{root_dir}/project_dir')
+
+with open(f'{src_dir}/local_dir/local.yaml', 'r') as f:
+	flat_scripts_dir = yaml.load(f, Loader=yaml.FullLoader)['flat_scripts_dir']
+
+subprocess.run([f'{flat_scripts_dir}/generate-qtproject.py',
+	'--config',      f'{src_dir}/ide_config_dir/config.yaml',
+	'--root-dir',    src_dir,
+	'--project-dir', project_dir,
+	'--local',       f'{src_dir}/local_dir/local.yaml'],
+	check=True)
